e9f2b6fd5214bfaef8632eb945fbd66bba9bcf9e
“NuongPTT” <nuongnunga8@gmail.com>
2023-11-19 16:27:57 +0700

    nuong khung

---
+++ demo-socket/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+  app.useWebSocketAdapter(new IoAdapter(app));
+  await app.listen(5000);
+}
+bootstrap();
